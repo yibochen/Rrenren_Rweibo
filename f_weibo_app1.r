83405084c974c617d5432de128941457bf1c94e9
yibochen <chenyibo.zjumath@gmail.com>
2012-12-19 07:36:00 +0800

    20121219-两段时间的关键词对比

--- f_weibo_app1.r
+++ f_weibo_app1.r
@@ -1,47 +1,69 @@
 
 
 # 微博关键词及时间分布
-f_weibo_app1 <- function(hisnick='chenyibo', scale_a=7, scale_b=1){
+f_weibo_app1 <- function(hisnick='chenyibo', 
+                         scale_a=7, scale_b=1, 
+                         cutday='2012-12-21'){
   load(paste('weibo_saved_', hisnick, '.RData', sep=''))
+  require(rmmseg4j)
+  require(wordcloud)
+  weibo_data <- weibo_get$weibo_data[order(
+    -as.numeric(as.POSIXlt(weibo_get$weibo_data$weibo_time))), ]
+  weibo_data_all <- sapply(strsplit(weibo_data$weibo_content, '//'), '[', 1)
+  flag <- min(which(as.POSIXlt(weibo_data$weibo_time) <= as.POSIXlt(cutday)))
+  weibo_data_1 <- sapply(strsplit(weibo_data$weibo_content[flag:nrow(weibo_data)], '//'), '[', 1)
+  weibo_data_2 <- sapply(strsplit(weibo_data$weibo_content[1:(flag-1)], '//'), '[', 1)
+  
+  if(length(weibo_data_1) > length(weibo_data_2)){
+    weibo_data_1 <- weibo_data_1[1:length(weibo_data_2)]
+  }
+  if(length(weibo_data_2) > length(weibo_data_1)){
+    weibo_data_2 <- weibo_data_2[(length(weibo_data_2)-length(weibo_data_1)+1):length(weibo_data_2)]
+  }
   
   # 分词
-  require(rmmseg4j)
-  weibo_data <- sapply(strsplit(weibo_get$weibo_data$weibo_content, '//'), '[', 1)
-  weibo_data <- weibo_data[weibo_data != '' & !is.na(weibo_data)]
-  f_cut <- function(x){
-    unlist(strsplit(mmseg4j(x), ' '))
+  f_fenci <- function(input=weibo_data){
+    weibo_data <- input[input != '' & !is.na(input)]
+    f_cut <- function(x){
+      unlist(strsplit(mmseg4j(x), ' '))
+    }
+    words <- unlist(lapply(weibo_data, f_cut))
+    words <- words[!words  %in% c('转发','回复')]
+    words[words == '联网'] <- '互联网'
+    words[words == '据分析'] <- '数据分析'
+    
+    # 统计词频
+    words_freq <- sort(table(words), dec=T)
+    words_names <- names(words_freq)
+    words_length <- nchar(words_names)
+    words_df <- data.frame(words_names=words_names, words_freq=words_freq, words_length=words_length)
+    # 只做两三个字的词，简单一点。。。
+    # words_df <- words_df[words_df$words_length %in% c(2,3), ]
+    
+    # 加载搜狗实验室的词频文件
+    SogouLabDic <- readLines('SogouLabDic.dic')
+    SogouLabDic <- paste(iconv(SogouLabDic, 'GBK', 'UTF-8'), ' ')
+    SogouLabDic <- data.frame(do.call(rbind, strsplit(SogouLabDic, '\t')), stringsAsFactors=F)
+    names(SogouLabDic)[1] <- 'words_names'
+    SogouLabDic_match <- SogouLabDic[SogouLabDic[,1] %in% words_df$words_names, ]
+    SogouLabDic_match$X2 <- as.numeric(SogouLabDic_match$X2)
+    
+    words_df2 <- merge (words_df, SogouLabDic_match, by='words_names', all.x=T)
+    # 可以筛选名词和动词。不过似乎没有必要，因为形容词副词什么的也能够体现用词风格嘛
+    # words_df2 <- words_df2[grep('^[NV],$',words_df2$X3), ]
+    
+    # 匹配不到的扔掉
+    words_df3 <- words_df2[!is.na(words_df2$X2), ]
+    words_df3$words_freq2 <- words_df3$words_freq * log(max(words_df3$X2)/words_df3$X2)
+    
+    return(words_df3)
   }
-  words <- unlist(lapply(weibo_data, f_cut))
-  words <- words[!words  %in% c('转发','回复')]
-  words[words == '联网'] <- '互联网'
-  words[words == '据分析'] <- '数据分析'
   
-  # 统计词频
-  words_freq <- sort(table(words), dec=T)
-  words_names <- names(words_freq)
-  words_length <- nchar(words_names)
-  words_df <- data.frame(words_names=words_names, words_freq=words_freq, words_length=words_length)
-  # 只做两三个字的词，简单一点。。。
-  # words_df <- words_df[words_df$words_length %in% c(2,3), ]
-  
-  # 加载搜狗实验室的词频文件
-  SogouLabDic <- readLines('SogouLabDic.dic')
-  SogouLabDic <- paste(iconv(SogouLabDic, 'GBK', 'UTF-8'), ' ')
-  SogouLabDic <- data.frame(do.call(rbind, strsplit(SogouLabDic, '\t')), stringsAsFactors=F)
-  names(SogouLabDic)[1] <- 'words_names'
-  SogouLabDic_match <- SogouLabDic[SogouLabDic[,1] %in% words_df$words_names, ]
-  SogouLabDic_match$X2 <- as.numeric(SogouLabDic_match$X2)
-  
-  words_df2 <- merge (words_df, SogouLabDic_match, by='words_names', all.x=T)
-  # 可以筛选名词和动词。不过似乎没有必要，因为形容词副词什么的也能够体现用词风格嘛
-  # words_df2 <- words_df2[grep('^[NV],$',words_df2$X3), ]
-  
-  # 匹配不到的扔掉
-  words_df3 <- words_df2[!is.na(words_df2$X2), ]
-  words_df3$words_freq2 <- words_df3$words_freq * log(max(words_df3$X2)/words_df3$X2)
-  
-  # 做词云（这个包貌似对中文支持不是很好）
-  require(wordcloud)
+  words_df3 <- f_fenci(weibo_data_all)
+  words_df1 <- f_fenci(weibo_data_1)
+  words_df2 <- f_fenci(weibo_data_2)
+  words_1 <- as.character(words_df1[order(-words_df1$words_freq2)[1:9], 1])
+  words_2 <- as.character(words_df2[order(-words_df2$words_freq2)[1:9], 1])
   
   png(paste('weibo_content_', hisnick, '_', Sys.Date(), '.png', sep=''),width=1000,height=1000)
   par(mfrow=c(2,2), mar=c(1,1,3,1), yaxt='n', 
@@ -57,12 +79,19 @@ f_weibo_app1 <- function(hisnick='chenyibo', scale_a=7, scale_b=1){
   wordcloud(words_df3$words_names, words_df3$words_freq2, min.freq=0, scale=c(scale_a, scale_b), 
             max.words=100, random.order=F, colors=rainbow(100,start=0.5,end=1), rot.per=0)
   plot(0, 0, type='n', xlim=c(0,100), ylim=c(0,100), axes=F)
-  text(50, 50, paste(weibo_get$hisnick, 
-                     substr(weibo_get$nick, 1, 5), 
-                     substr(weibo_get$nick, 6, 10), 
-                     substr(weibo_get$nick, 11, 100), 
-                     sep='\n'), 
-       cex=6, font=2, col=rgb(0,0.3,1))
+  text(0, 50, paste(weibo_get$nick, '\n', 
+                    '在', cutday, '之前的\n', 
+                    length(weibo_data_1), '条微博的关键词是：\n', 
+                    paste(c(words_1[1:3], ''), collapse=';'), '\n', 
+                    paste(c(words_1[4:6], ''), collapse=';'), '\n', 
+                    paste(c(words_1[7:9]), collapse=';'), '\n\n', 
+                    '在', cutday, '之后的\n', 
+                    length(weibo_data_2), '条微博的关键词是：\n', 
+                    paste(c(words_2[1:3], ''), collapse=';'), '\n', 
+                    paste(c(words_2[4:6], ''), collapse=';'), '\n', 
+                    paste(c(words_2[7:9]), collapse=';'), 
+                    sep=''), 
+       cex=3, font=2, adj=0, col=rgb(0,0.3,1))
   dev.off()
 }
 
