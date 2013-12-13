67ab784927070a9c7f2a7ac9097af85a514f475f
liuzheng712 <liuzheng712@gmail.com>
2013-12-13 22:40:50 +0800

    RSA加密由于位数限制无法实现，无加密表单完成，但是postForm请求有问题，始终无法成功post

--- f_weibo_login.r
+++ f_weibo_login.r
@@ -37,46 +37,86 @@ f_weibo_login <- function(name='****', pwd='****'){
                       cookiejar='./cookies', cookiefile='./cookies')
   
   # 预登录
-  preurl <- paste('http://login.sina.com.cn/sso/prelogin.php?entry=miniblog&callback=sinaSSOController.preloginCallBack&su=', 
-                  name2, '&client=ssologin.js(v1.3.18)', sep='')
+
+
+###########RSA加密###################################
+#library(base64)
+#su <- base64Encode(name)[1]
+#url_prelogin <- 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=&rsakt=mod&client=ssologin.js(v1.4.5)&_=1364875106625'
+#url_login <- 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.5)'
+#resp <- getURL(url_prelogin, curl=cH)
+#json_data <- substr(resp,regexpr("\\(",resp)+1,regexpr("\\)",resp)-1)
+#data <- fromJSON(json_data)
+#servertime <- data$servertime
+#nonce <- data$nonce
+#pubkey <- data$pubkey
+#rsakv <- data$rsakv
+# 以下超strtio限了，无法对pubkey进行十进制化
+#rsaPublickey <- strtoi(paste('0x',pubkey,sep=''))
+# 接下来需要对sp进行rsa加密，无法去实现了。。。
+
+
+  preurl <- 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=&rsakt=mod&client=ssologin.js(v1.4.5)'
   prelogin <- getURL(preurl, curl=cH)
   preinfo <- fromJSON(gsub('^.*\\((.*)\\).*$','\\1',prelogin))
   servertime <- preinfo$servertime
-  pcid <- preinfo$pcid
-  nonce <- preinfo$nonce
+#  pcid <- preinfo$pcid
+#  nonce <- preinfo$nonce
   # 加密的过程
-  pwd1 <- digest(pwd, algo='sha1', seria=F)
-  pwd2 <- digest(pwd1, algo='sha1', seria=F)
-  pwd3 <- digest(paste(pwd2, servertime, nonce, sep=''), algo='sha1', seria=F)
-  pinfo=c(
-    'service'='miniblog',
-    'client'='ssologin.js(v1.3.18)',
-    'entry'='weibo',
-    'encoding'='UTF-8',
-    'gateway'='1',
-    'savestate'='7',
-    'from'='',
-    'useticket'='1',
-    'su'=name2,
-    'servertime'=servertime,
-    'nonce'=nonce,
-    'pwencode'='wsse',
-    'sp'=pwd3,
-    'vsnf'='1',
-    'vsnval'='',
-    'pcid'=pcid,
-    'url'='http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
-    'returntype'='META',
-    'ssosimplelogin'='1',
-    'setdomain'='1')
+#  pwd1 <- digest(pwd, algo='sha1', seria=F)
+#  pwd2 <- digest(pwd1, algo='sha1', seria=F)
+#  pwd3 <- digest(paste(pwd2, servertime, nonce, sep=''), algo='sha1', seria=F)
+#  pinfo=c(
+#   'service'='miniblog',
+#    'client'='ssologin.js(v1.3.18)',
+#    'entry'='weibo',
+#    'encoding'='UTF-8',
+#    'gateway'='1',
+#    'savestate'='7',
+#    'from'='',
+#    'useticket'='1',
+#    'su'=name2,
+#    'servertime'=servertime,
+#    'nonce'=nonce,
+#    'pwencode'='wsse',
+#    'sp'=pwd3,
+#    'vsnf'='1',
+#    'vsnval'='',
+#    'pcid'=pcid,
+#    'url'='http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
+#    'returntype'='META',
+#    'ssosimplelogin'='1',
+#    'setdomain'='1')
+################无加密部分#####################
+pinfo=c(
+entry = 'sso',
+gateway = '1',
+from = '',
+savestate = '30',
+useticket = '0',
+pagerefer = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.13)',
+vsnf = '1',
+su= name2,
+service = 'sso',
+sp = pwd,
+sr = '1280*1024',
+encoding = 'UTF-8',
+prelt = '0',
+callback = 'parent.sinaSSOController.loginCallBack',
+returntype = 'IFRAME',
+setdomain = '1')
+###############################################
   # 登录
   bkp_ctype <- Sys.getlocale('LC_CTYPE')
   if(bkp_ctype == 'zh_CN.UTF-8'){Sys.setlocale('LC_CTYPE', 'C')}
-  x <- try(ttt <- postForm('http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.3.18)', 
-                           .params=pinfo, curl=cH, style='post'), silent=T)
+  x <- try(ttt <- postForm('http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.13)', 
+                           .params=pinfo, curl=cH, style='POST'), silent=T)
   if(class(x) == 'try-error'){cat('no!!!!!!');return(NULL)}
-  newurl <- gsub('^.*location.replace\\(\'(.+)\'\\);.*$', '\\1', ttt[1])
+print(ttt[1])
+  newurl <- gsub('location.replace\\(\'(.*?)\"\\)', '\\1', ttt[1])
   x <- try(x <- getURL(newurl, curl=cH, .encoding='UTF-8'), silent=T)
+#print(x)
+#print(class(x))
   Sys.setlocale('LC_CTYPE', bkp_ctype)
   if(class(x) == 'try-error'){cat('no!!!!!!');return(NULL)}
   getCurlInfo(cH)[['cookielist']]
